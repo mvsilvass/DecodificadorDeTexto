67e883e5613229cb7a7aad29b5107f3e3bf6486d
Maria Vitoria <mvsilvasoaress@gmail.com>
2024-07-08 17:07:38 -0300

    :construction:  uptade script.js

--- scripts/script.js
+++ scripts/script.js
@@ -4,20 +4,33 @@ document.addEventListener("DOMContentLoaded", () => {
 
     function criptografar() {
         let text = textArea.value;
-
+    
+        // Validando se há caracteres maiúsculos ou especiais
+        if (/[A-Z!@#$%^&*()_+{}\[\]:;"'<>,.?\/\\|áéíóúâêîôûãõàèìòùäëïöü]/i.test(text)) {
+            alert("Apenas letras minúsculas sem acentos ou caracteres especiais são permitidas.");
+            return;
+        }
+    
+        // Aplicando a criptografia
         text = text.replace(/e/g, "enter");
         text = text.replace(/i/g, "imes");
         text = text.replace(/a/g, "ai");
         text = text.replace(/o/g, "ober");
         text = text.replace(/u/g, "ufat");
-
+    
         // Exibindo o resultado
         resultArea.innerHTML = `<h2>Mensagem Criptografada</h2><p>${text}</p>`;
     }
 
     function descriptografar() {
         let text = textArea.value;
-
+    
+        // Validando se há caracteres maiúsculos ou especiais
+        if (/[A-Z!@#$%^&*()_+{}\[\]:;"'<>,.?\/\\|áéíóúâêîôûãõàèìòùäëïöü]/i.test(text)) {
+            alert("Apenas letras minúsculas sem acentos ou caracteres especiais são permitidas.");
+            return;
+        }
+    
         text = text.replace(/enter/g, "e");
         text = text.replace(/imes/g, "i");
         text = text.replace(/ai/g, "a");
